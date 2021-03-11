47a6846bc8e9bee7a1ef7e16601fdac951f72e05
Lucy <luci.port@gmail.com>
2021-03-11 14:52:44 -0400

    add quesiton component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { WebSpeechModule } from './web-speech/web-speech.module';
 import { TutorialsMenuComponent } from './tutorials-menu/tutorials-menu.component';
+import { QuestionPageComponent } from './question-page/question-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TutorialsMenuComponent
+    TutorialsMenuComponent,
+    QuestionPageComponent
   ],
   imports: [
     BrowserModule,
