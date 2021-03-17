d63649d0b79508a1769bbc47181b0c0d2bb39df3
Lucy <luci.port@gmail.com>
2021-03-17 15:46:14 -0400

    capture text in speech component/commence tutorial component

--- src/app/shared/services/web-apis/speech-recognition.service.ts
+++ src/app/shared/services/web-apis/speech-recognition.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { NgIfContext } from '@angular/common';
+import { Injectable, NgZone } from '@angular/core';
+import { Observable } from 'rxjs';
 declare var webkitSpeechRecognition: any;
 @Injectable({
   providedIn: 'root'
@@ -6,27 +8,52 @@ declare var webkitSpeechRecognition: any;
 
 
 export class SpeechRecognitionService {
-  recognition =  new webkitSpeechRecognition();
-  isListening = false; 
-  constructor() { }
+  recognition = new webkitSpeechRecognition();
+  isListening = false;
+  finalStatement: Observable<string>;
+
+  constructor(private ngZone: NgZone) { }
 
   initialize(): void {
-      this.recognition.lang = 'en-US';
-      this.recognition.continuous = true; 
-      this.recognition.interimResults = true; 
-      console.log("Speech recognition intialized");
+    this.recognition.lang = 'en-US';
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    console.log("Speech recognition intialized");
   }
 
   start(): void {
-    this.isListening = true; 
+    this.isListening = true;
     this.recognition.start();
     console.log("Speech recognition started");
   }
 
   stop(): void {
-    this.isListening = false; 
+    this.isListening = false;
     this.recognition.stop();
-    console.log("Speech recognition stopped"); 
+    console.log("Speech recognition stopped");
+  }
+
+  onResult(): Observable<string> {
+    return new Observable(observer => {
+      this.recognition.onresult = (event: SpeechRecognitionEvent) => {
+        console.log("Speech recognition service onresult", event);
+        let words = " ";
+        let tempWords = " ";
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            words += event.results[i][0].transcript;
+            this.ngZone.run(() => observer.next(words));
+          }
+          else {
+            tempWords += event.results[i][0].transcript;
+            this.ngZone.run(() => observer.next(tempWords))
+          }
+        }
+        console.log(`words: ${words}`);
+        console.log("tempwords", tempWords);
+      }
+    }
+    );
   }
 
 }
--- src/app/speech/speech.component.ts
+++ src/app/speech/speech.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEventPattern } from 'rxjs';
 import { SpeechRecognitionService } from '../shared/services/web-apis/speech-recognition.service';
+import { Observable } from 'rxjs'; 
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-speech',
@@ -8,10 +11,13 @@ import { SpeechRecognitionService } from '../shared/services/web-apis/speech-rec
 })
 export class SpeechComponent implements OnInit {
 
-  constructor(private speechRecognition: SpeechRecognitionService) { }
+  transcript?: Observable<string>; 
+
+  constructor(public speechRecognition: SpeechRecognitionService) { }
 
   ngOnInit(): void {
     this.speechRecognition.initialize();
+    this.captureText();  
   }
   start():void {
     this.speechRecognition.start(); 
@@ -19,6 +25,14 @@ export class SpeechComponent implements OnInit {
 
   stop():void {
     this.speechRecognition.stop(); 
+  }
+  captureText(): void {
+    console.log("component initialized"); 
+    this.transcript = this.speechRecognition.onResult()//.pipe(
+  //    map((notification) => {
+  //      console.log(`notification 1${notification}`);
+  //      return notification;}));
+    console.log("transcript", this.transcript);
   }
 }
 
